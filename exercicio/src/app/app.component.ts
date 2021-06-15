346e3a747873ea5f448528530c708e5a1efc7bf4
jotagehes <62728338+jotagehes@users.noreply.github.com>
2021-06-15 19:59:10 -0300

    initial commit

---
+++ exercicio/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  public appPages = [
+    { title: 'Home', url: '/folder/Inbox', icon: 'home' },
+    { title: 'Alunos', url: '/folder/Outbox', icon: 'person-outline' },
+  ];
+  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  constructor() {}
+}
